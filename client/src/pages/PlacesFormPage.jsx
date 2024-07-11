08c9d678404a830991370daa14b0b4dcd934d84f
Gabe <gabriel.haro.03@gmail.com>
2024-07-11 13:32:28 -0500

    Listing places on homepage

--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -17,6 +17,7 @@ export default function PlacesFormPage() {
     const [checkIn,setCheckIn] = useState('');
     const [checkOut,setCheckOut] = useState('');
     const [maxGuests,setMaxGuests] = useState(1);
+    const [price,setPrice] = useState(100);
     const [redirect,setRedirect] = useState(false);
     useEffect(() => {
         if (!id) {
@@ -33,6 +34,7 @@ export default function PlacesFormPage() {
             setCheckIn(data.checkIn);
             setCheckOut(data.checkOut);
             setMaxGuests(data.maxGuests);
+            setPrice(data.price);
 
         });
     }, [id]);
@@ -60,7 +62,7 @@ export default function PlacesFormPage() {
         const placeData = {
             title, address, addedPhotos, 
             description, perks, extraInfo, 
-            checkIn, checkOut, maxGuests
+            checkIn, checkOut, maxGuests, price,
         };
         if (id) {
             //update
@@ -99,7 +101,7 @@ export default function PlacesFormPage() {
                 {preInput('Extra info', 'house rules, etc')}
                 <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)}/>
                 {preInput('Check in&out times', 'add check in and out times, remember to have some time window for cleaning the room between guests')}
-                <div className="grid gap-2 sm:grid-cols-3">
+                <div className="grid gap-2 grid-cols-2 md-grid-cols-4">
                     <div>
                         <h3 className="mt-2 -mb-1">Check in time</h3>
                         <input type="text" 
@@ -118,6 +120,11 @@ export default function PlacesFormPage() {
                         <h3 className="mt-2 -mb-1">Max number of guests</h3>
                         <input type="number" value={maxGuests} 
                             onChange={ev => setMaxGuests(ev.target.value)}/>
+                    </div>
+                    <div>
+                        <h3 className="mt-2 -mb-1">Price per night</h3>
+                        <input type="number" value={price} 
+                            onChange={ev => setPrice(ev.target.value)}/>
                     </div>
                 </div>                      
                 <button className="primary my-4">Save</button>
